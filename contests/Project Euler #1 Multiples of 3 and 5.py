02587ee5ad40f37c25524ff79e05bfa9053ce223
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-20 22:21:14 +0700

    add: contests folder and also 3 and 5 multiply contest file

---
+++ contests/Project Euler #1 Multiples of 3 and 5.py
@@ -0,0 +1,10 @@
+def sum_of_arithmetic_sequence(n, factor):
+    t = n // factor # Find the maximum times
+    s = t*(factor+factor*t) // 2 # Adapt from sum of arithmetic sequence formula
+    return s
+
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())-1 
+    result = sum_of_arithmetic_sequence(n, 3) + sum_of_arithmetic_sequence(n, 5) - sum_of_arithmetic_sequence(n, 15) 
+    print(int(result))
