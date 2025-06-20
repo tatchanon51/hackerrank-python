fd7ec127ac1beab99ca5c3d7391ec6885850d7e2
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-20 22:25:12 +0700

    Update: Problem Link

--- contests/Project Euler #1 Multiples of 3 and 5.py
+++ contests/Project Euler #1 Multiples of 3 and 5.py
@@ -1,3 +1,5 @@
+# Problem Link: https://www.hackerrank.com/contests/projecteuler/challenges/euler001/
+
 def sum_of_arithmetic_sequence(n, factor):
     t = n // factor # Find the maximum times
     s = t*(factor+factor*t) // 2 # Adapt from sum of arithmetic sequence formula
