8a80b162b523db44f5b8b901b2c961d50c5fc2c2
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-21 09:20:25 +0700

    adding: Arithmetic Operators.py

---
+++ Prepare/Introduction/Arithmetic Operators.py
@@ -0,0 +1,8 @@
+# Problem Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())    # read input and set var a
+    b = int(input())    # read input and set var b
+    print(a+b)          # calculation and print
+    print(a-b)          
+    print(a*b)
