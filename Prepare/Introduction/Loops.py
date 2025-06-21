2b36e840604930fff1ea0d480cac31bf257d175c
tatchanon51 <tatchanon51@gmail.com>
2025-06-21 20:57:52 +0700

    add: Loops.py

---
+++ Prepare/Introduction/Loops.py
@@ -0,0 +1,6 @@
+# Problem Link: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())        # Read input
+    for x in range(n):      # Iterate int < n
+        print(x*x)
