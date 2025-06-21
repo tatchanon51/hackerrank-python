f6378535edf5bda0ef4e1908ab2ebfcc2aaa1402
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-21 09:23:58 +0700

    adding: Python Division.py

---
+++ Prepare/Introduction/Python Division.py
@@ -0,0 +1,7 @@
+# Problem Link: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())    # read input
+    b = int(input())
+    print(a//b)         # Floor division
+    print(a/b)          # True division
