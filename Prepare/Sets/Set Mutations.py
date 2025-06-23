9acccf6abbd905ead3cb074743e9416c63b39a48
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-06-23 21:41:18 +0700

    Update Set Mutations.py

--- Prepare/Sets/Set Mutations.py
+++ Prepare/Sets/Set Mutations.py
@@ -9,12 +9,12 @@ for i in range(com_num):                                #Iterate thru all comman
     n = int(com[1])                                     #Get the number of operating set
     com = com[0]                                        #Get only str from the command
     op_set = set(map(int,input().split()))              #Get the operating set in each command
-    if 'intersection_update' in com:                    #Check if the command is intersection_update
+    if 'intersection_update' == com:                    #Check if the command is intersection_update
         st_set.intersection_update(op_set)              
-    if 'update' in com:                                 #Check if the command is update
+    if 'update' == com:                                 #Check if the command is update
         st_set.update(op_set)
-    if 'symmetric_difference_update' in com:            #Check if the command is symmetric_difference_update
+    if 'symmetric_difference_update' == com:            #Check if the command is symmetric_difference_update
         st_set.symmetric_difference_update(op_set)
-    if 'difference_update' in com:                      #Check if the command is difference_update
+    if 'difference_update' == com:                      #Check if the command is difference_update
         st_set.difference_update(op_set)
 print(sum(st_set))                                      #Print sum of the mutated set
