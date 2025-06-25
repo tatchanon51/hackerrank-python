20a6aefd99885948b11d558ab67bac7c18986225
tatchanon51 <tatchanon51@gmail.com>
2025-06-25 10:24:57 +0700

    Create Prepare/Math/Integers Come In All Sizes.py

---
+++ Prepare/Math/Integers Come In All Sizes.py
@@ -0,0 +1,8 @@
+# Problem Link: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print((a**b)+(c**d))
