c017df6762034c0d9539f5e5163770883246a618
tatchanon51 <tatchanon51@gmail.com>
2025-06-25 10:25:25 +0700

    Prepare/Math/Power - Mod Power.py

---
+++ Prepare/Math/Power - Mod Power.py
@@ -0,0 +1,8 @@
+#Problem Link: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b))
+print(pow(a,b,m))
