c148f803c16cfc2b7f14c064ac82b1c7eaf4fee2
Tatchanon Wissawakornrungrot <tatchanon51@gmail.com>
2025-07-05 20:33:19 +0700

    Update Collections.namedtuple().py

--- Prepare/Collections/Collections.namedtuple().py
+++ Prepare/Collections/Collections.namedtuple().py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  3 22:43:01 2025
-
-@author: BaBuu
-"""
-
+# Problem Link: https://www.hackerrank.com/challenges/py-collections-namedtuple/problem?isFullScreen=true
+from collections import namedtuple
+N,stu = int(input()), namedtuple('Student',input().split())         #setup all field name
+print(sum([int(stu(*input().split()).MARKS) for _ in range(N)])/N)                 #iterate thru all students and get MARK value
+'''
+from collections import namedtuple
+N = int(input())
+stu = namedtuple('Student',input().split())         #setup all field name
+mark = []
+for i in range(N):                                  #iterate thru all students and get MARK value
+    mark.append(int(stu(*input().split()).MARKS))
+print(sum(mark)/len(mark))'''
