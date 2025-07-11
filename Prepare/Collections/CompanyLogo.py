a1f6544c6e109f83a23cd8a3cb06c4a4272de00e
tatchanon51 <tatchanon51@gmail.com>
2025-07-11 14:34:50 +0700

    Create CompanyLogo.py

---
+++ Prepare/Collections/CompanyLogo.py
@@ -0,0 +1,12 @@
+# Problem Link: https://www.hackerrank.com/challenges/most-commons/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    s = input()
